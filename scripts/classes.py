9535bc76c418566b6a70af3f4ead97f80a07ccc4
Kalapimea <atsukka2011@gmail.com>
2026-06-19 20:29:34 +0300

    updated folder structure such that commands are seperate from main file for real this time...

---
+++ scripts/classes.py
@@ -0,0 +1,51 @@
+from scripts.generators.classes_gen import *
+
+def classes(command):
+    if command[1] == "help":
+        print("""help> use 'class list' to list all classes,
+              if nothing shows up, use 'class generate default' to generate default configuration of classes to classes.json
+              or 'class generate custom' for a customied generation
+              or 'class generate reset' to empty out classes.json (you do not need to reset before the other commands)
+              
+              use 'class info <class> <info>' to see information about class.
+              
+              <class> takes any of the classes seen in  'class list' as input
+              <info> takes one of the following to show about <class>:
+                teacher        shows name of teacher
+                room           shows room number of class""")
+    
+    elif command[1] == "list":
+        with open("data/classes.json", "r") as f:
+            data = json.load(f)
+        
+        for subject in data:
+            print(subject["class_name"])
+    
+    elif command[1] == "generate":
+        if len(command) > 2:
+            if command[2] == "default":
+                generate_classes()
+            elif command[2] == "custom":
+                generate_classes_custom()
+            elif command[2] == "empty":
+                reset_configuration()  
+
+    elif command[1] == "info":
+        filter_class = command[2]
+        info = command[3]
+
+        with open("data/classes.json", "r") as f:
+            subjects = json.load(f)
+
+            for subject in subjects:
+                if subject["class_name"] == filter_class:
+                    if info == "teacher":
+                        print(f"Teacher of {subject["class_name"]} is {subject["class_teacher"]}")
+                    elif info == "room":
+                        print(f"Room number of {subject["class_name"]} is {subject["class_room_number"]}")
+                    else:
+                        print("invalid info flag of {info}")
+                    
+                    break
+            else:
+                print(f"No such class '{filter_class}' found")
---
+++ scripts/generators/classes_gen.py
@@ -0,0 +1,78 @@
+import json
+
+def generate_classes():
+
+    classes = ["math", "chemistry", "history", "religion", "physics", "english", "swedish", "german", "finnish", "biology", "geography", "health-education"]
+
+    classes.sort()
+
+    subjects = []
+
+    for subject in classes:
+        class_name = subject
+        class_teacher = "nobody"
+        class_room_number = "A000"
+
+        classes_dict = {
+            "class_name": class_name,
+            "class_teacher": class_teacher,
+            "class_room_number": class_room_number
+        }
+
+        subjects.append(classes_dict)
+
+    with open("data/classes.json", "w") as f:
+        json.dump(subjects, f, indent=4)
+
+    print("Succesfully configured classes.json!")
+
+def generate_classes_custom():
+    if input("do you want to use your own list of subjects instead of the predifined one (yes/no): ").lower == "yes":
+        classes = ["math", "chemistry", "history", "religion", "physics", "english", "swedish", "german", "finnish", "biology", "geography", "health-education"]
+    else:
+        classes = []
+
+        for i in range(int(input("how many classes do you have?: "))):
+            classes.append(input(f"enter name of class number {i+1}: "))
+    
+    classes.sort()
+
+    subjects = []
+    for subject in classes:
+        classes_dict = {
+            "class_name": subject,
+            "class_teacher": input(f"enter name of your {subject} teacher: "),
+            "class_room_number": input(f"enter class room number of {subject}: ")
+        }
+
+        subjects.append(classes_dict)
+
+    with open("data/classes.json", "w") as f:
+        json.dump(subjects, f, indent=4)
+    
+    print("succesfully generated custom configuration to classes.json!")
+
+def reset_configuration():
+    subjects = []
+
+    with open("data/classes.json", "w") as f:
+        json.dump(subjects, f, indent=4)
+
+if __name__ == "__main__":
+    operation = int(input("""what do you want to do?
+                          1. generate default configuration
+                          2. generate custom configuration
+                          3. generate empty confuguration
+                          enter number of operation: """))
+    
+    if operation == 1:
+        generate_classes()
+        print("succesfully generated defaults")
+    elif operation == 2:
+        generate_classes_custom()
+        print("succesfully generated custom")
+    elif operation == 3:
+        reset_configuration()
+        print("succesfully generated empty")
+    else:
+        print("invalid operation")
