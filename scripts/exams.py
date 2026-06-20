1270700e0f8154075ae01ae19afeac0077f5bddd
Kalapimea <atsukka2011@gmail.com>
2026-06-20 13:09:35 +0300

    added showing and filtering for upcoming exams

--- scripts/exams.py
+++ scripts/exams.py
@@ -4,7 +4,8 @@ from scripts.helpers.dates import *
 
 def exams(command):
     if command[1] == "help":
-        print("""exam add <class> <yyyy-mm-dd> <hh:mm>""")
+        print("""exam add <class> <yyyy-mm-dd> <hh:mm>
+              exam show [yyyy-mm-dd]""")
     
     elif command[1] == "add":
         if len(command) >= 5:
@@ -26,3 +27,17 @@ def exams(command):
 
             with open("data/exams.json", "w") as f:
                 json.dump(exams, f, indent=4)
+
+    elif command[1] == "show":
+        with open("data/exams.json", "r") as f:
+            exams = json.load(f)
+
+        if len(command) > 2:
+            date_filter = command[2]
+
+            for exam in exams:
+                if exam["exam_date"] == date_filter:
+                    print(f"{exam["exam_class"]} exam on {exam["exam_date"]} ({convert_to_weekday(exam["exam_date"])}) at {exam["exam_time"]}")
+        else:
+            for exam in exams:
+                print(f"{exam["exam_class"]} on {exam["exam_date"]} ({convert_to_weekday(exam["exam_date"])}) at {exam["exam_time"]}")
