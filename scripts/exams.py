1336ffe5698580764260bc4893856015e4cd831e
Kalapimea <atsukka2011@gmail.com>
2026-06-20 12:15:13 +0300

    fixed adding exams

--- scripts/exams.py
+++ scripts/exams.py
@@ -5,6 +5,24 @@ from scripts.helpers.dates import *
 def exams(command):
     if command[1] == "help":
         print("""exam add <class> <yyyy-mm-dd> <hh:mm>""")
+    
     elif command[1] == "add":
-       pass
-       
+        if len(command) >= 5:
+            exam_class = command[2]
+            exam_date = command[3]
+            exam_time = command[4]
+
+            
+            with open("data/exams.json", "r") as f:
+                exams = json.load(f)
+            
+            exam_entry = {
+                "exam_class": exam_class,
+                "exam_date": exam_date,
+                "exam_time": exam_time
+            }
+            
+            exams.append(exam_entry)
+
+            with open("data/exams.json", "w") as f:
+                json.dump(exams, f, indent=4)
