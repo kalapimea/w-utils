d38ea32053561372fd4e1788a2ba3b5b3f6416ed
Kalapimea <atsukka2011@gmail.com>
2026-06-22 17:12:42 +0300

    added way to view past exams using 'exam show past'

--- scripts/exams.py
+++ scripts/exams.py
@@ -7,7 +7,8 @@ def exams(command):
         print("""exam add <class> <yyyy-mm-dd> <hh:mm>
               exam show [--flag] [filter]
               exam update
-              flags --status/-s --date/-d""")
+              flags --status/-s --date/-d
+              exam show past [--flag] [filter]""")
     
     elif command[1] == "add":
         if len(command) >= 5:
@@ -31,7 +32,28 @@ def exams(command):
 
             with open("data/exams.json", "w") as f:
                 json.dump(exams, f, indent=4)
+    
+    elif command[1] == "show" and command[2] == "past":   #dont question it... i added this way after the other show so i couldent fit it there nicely
+        with open("data/exams_past.json", "r") as f:
+            exams_past = json.load(f)
 
+        if len(command) >= 5:
+            if command[3] == "--date" or command[3] == "-d":
+                filter_date = command[4]
+            elif command[3] == "--subject" or command[3] == "-s":
+                filter_subject = command[4]
+            else:
+                filter_date = ""
+                filter_subject = ""
+            
+            for exam in exams_past:
+                if exam["exam_date"] == filter_date or exam["exam_class"] == filter_subject:
+                    print(f"{exam["exam_class"]} exam was on {exam["exam_date"]}")
+        for exam in exams_past:
+            print(f"{exam["exam_class"]} exam was on {exam["exam_date"]}")
+            
+        
+    
     elif command[1] == "show":
         with open("data/exams.json", "r") as f:
             exams = json.load(f)
@@ -39,13 +61,12 @@ def exams(command):
         date_filter = ""
         status_filter = ""
         
-        if len(command) > 2:
+        if len(command) >= 4:
             if command[2] == "--date" or command[2] == "-d":
                 date_filter = command[3]
             elif command[2] == "--status" or "-s":
                 status_filter = command[3]
-
-
+           
             for exam in exams:
                 if exam["exam_date"] == date_filter or exam["exam_status"] == status_filter:
                     print(f"{exam["exam_class"]} exam on {exam["exam_date"]} ({convert_to_weekday(exam["exam_date"])}) at {exam["exam_time"]}")
